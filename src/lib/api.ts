7411641bdb889a352818795e6f0076e31de15f32
Kaarthik-07 <57kaarthikj@gmail.com>
2025-11-06 17:46:24 +0530

    [fix]-> domain url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,9 +3,10 @@ import { getToken } from "./auth";
 
 //url
 const api = axios.create({
-    baseURL: "https://krown-server.onrender.com/api",
+    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_API}/api`,
 });
 
+console.log("Backend API:", process.env.NEXT_PUBLIC_BACKEND_API);
 // Automatically inject token before every request
 api.interceptors.request.use((config) => {
     const token = getToken(); // from localStorage
